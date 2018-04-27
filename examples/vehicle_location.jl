a0d63466f05a5e7c52c004b3935382997fd9a512
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-04-27 13:08:02 -0400

    glpk on travis makes more prorgress on vehicle example, don't test bound exactly

--- examples/vehicle_location.jl
+++ examples/vehicle_location.jl
@@ -109,7 +109,7 @@ end
 srand(1234)
 ambulancemodel = vehiclelocationmodel(3, [0, 20, 40, 60, 80, 100], 0:10:100)
 @assert solve(ambulancemodel, max_iterations=20) == :max_iterations
-@test isapprox(getbound(ambulancemodel), 1206.0, atol=5)
+@test getbound(ambulancemodel) >= 1206.0
 
 # Symmetry breaking constraints
 # @variables(sp, begin
